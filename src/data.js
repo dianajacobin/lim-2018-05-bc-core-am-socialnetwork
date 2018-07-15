410a83ac8a3e7475450d71527628dcc7e3630095
RosemarieLopezH <rosemarie61@gmail.com>
2018-07-15 12:52:47 -0500

    registro de usuarios

--- src/data.js
+++ src/data.js
@@ -1,9 +1,11 @@
 const registrar = () => {
-    let email = document.getElementById('email').value,
+    let email = document.getElementById('email').value; //estamos rescatando el valor del id del input, se jaara del html y se guardaen la varible
     password = document.getElementById('password').value;
 
     // Registrar Usuario FIREBASE
-    firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+    //hace una promesa cath, que si no funciona se le pasa una funcion con el ´parametro error
+    .catch(function(error) { 
         // Handle Errors here.
         var errorCode = error.code;
         var errorMessage = error.message;
