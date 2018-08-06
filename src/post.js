20fd8204f63410ce3618853c8afffb577a6c29ad
dianajacobin <dianajacobin@gmail.com>
2018-08-06 11:05:25 -0500

    funcion privacidad

--- src/post.js
+++ src/post.js
@@ -18,41 +18,7 @@ let updates = {};
   return firebase.database().ref().update(updates);
 }
 
-//Eliminar post
-const deletePost = (newPostKey) => {
- if (confirm('¿Estas Seguro de Eliminar tu publicaión?'))
-    {
-        const uid = firebase.auth().currentUser.uid;
-        firebase.database().ref().child('/posts/' + newPostKey).remove();
-        firebase.database().ref().child('/user-posts/' + uid + '/' + newPostKey).remove();
-        while (posts.firstChild)posts.removeChild(posts.firstChild);
-        window.location.reload();
-    }
-else {alert('Se procedió a cancelar la eliminación');}  
-}
-
-// likes
-
-
-// pintar las publicaciones
-const showPost = () => { 
-    firebase.database().ref('posts/')
-    .on('value', (postsRef) =>{
-      const posts = postsRef.val();
-      const content = document.getElementById('content');
-      content.innerHTML='';
-      let userId = firebase.auth().currentUser.uid;
-      const crud = (id) => {
-        return `<div class="crud">
-        <button onclick="postEdit('${id}')" class="btnSave"></button>
-        <button onclick="editPost('${id}')" class="btnEdit"></button>
-        <button onclick="deletePost('${id}')" id="btnDelete"></button>
-        </div>`
-      }
-    })
-    ;
-}
-
+// show the post
 const listAllPost = () => {
   let data = '';
   let postRef = firebase.database().ref('posts/');
@@ -61,34 +27,41 @@ const listAllPost = () => {
     const postKeys = Object.keys(posts);
 
     postKeys.forEach( post => {
-      
-       data += `
-                <div class="row justify-content-center hiden postSolo">
-                  <div class="col-2">
-                    <img id="userImage" src="${posts[post].photoURL}" height="44" width="44" >
-                  </div>
-                  <div class="col-10">
-                    <form action="">
-                        <div class="row divTextArea">
-                            <textarea  id="post" name="Publicacion" class="form-control input-contrast comment-form-textarea">
-                              ${posts[post].mensaje}
-                            </textarea>
-                        </div>
-                        <div class="row divTextAreaActions">
-                            <div class="col-12">
-                                <input id="btnEditar" type="button" class="btn btn-primary" value="Editar">
-                                <input id="btnEliminar" type="button" class="btn btn-primary" value="Eliminar">
-                            </div>
-                        </div>
-                    </form>
-                    </div>  
-                  </div>  
-                  
-          `;
 
+      
+          if(posts[post].isPublic){
+              
+              data += `
+                        <div class="row justify-content-center hiden postSolo">
+                          <div class="col-2">
+                            <img id="userImage" src="${posts[post].photoURL}" height="44" width="44" >
+                          </div>
+                          <div class="col-10">
+                            <form action="">
+                                <div class="row divTextArea">
+                                    <textarea  name="Publicacion" class="form-control input-contrast comment-form-textarea">
+                                      ${posts[post].mensaje}
+                                    </textarea>
+                                </div>
+                                <div class="row divTextAreaActions">
+                                    <div class="col-12">
+                                        <input type="button" class="btn btn-primary" value="Editar">
+                                        <input type="button" class="btn btn-primary" value="Eliminar">
+                                    </div>
+                                </div>
+                            </form>
+                            </div>  
+                          </div>  
+                          
+                  `;
+          }
+       
     });
     let allPosts = document.getElementById('allPosts');
     allPosts.innerHTML = data;
+
+
+  
 
   });
 
