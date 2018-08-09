143a5c56861f03beeb4bee6cf028092a7e7fdac9
dianajacobin <dianajacobin@gmail.com>
2018-08-09 17:05:16 -0500

    Agregando archivo firebase.js

---
+++ src/js/firebase.js
@@ -0,0 +1,9 @@
+const config = {
+    apiKey: "AIzaSyDlyPsTR7Jo4y0aSUIipN_NooO8HtqJexY",
+    authDomain: "fir-c6242.firebaseapp.com",
+    databaseURL: "https://fir-c6242.firebaseio.com",
+    projectId: "fir-c6242",
+    storageBucket: "fir-c6242.appspot.com",
+    messagingSenderId: "482129686898"
+  };
+  firebase.initializeApp(config);
